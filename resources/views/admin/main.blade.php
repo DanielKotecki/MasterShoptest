24a5cd60693ccf566bdd04daa55a8786e794b9d9
Daniel <daniel.kotecki98@gmail.com>
2022-07-24 18:55:00 +0200

    add ioicon

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -114,5 +114,7 @@
 <script src="{{asset('dist/js/demo.js')}}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="{{asset('dist/js/pages/dashboard.js')}}"></script>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
